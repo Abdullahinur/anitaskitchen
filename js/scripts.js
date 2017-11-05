1d85cb35d46802f12753a28a8fa1c8e108e270e5
Abdullahinur <aabdullahinur@hotmail.com>
2017-11-05 14:14:21 +0300

    add hamburger menu reformatted with id linefeft

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,10 @@ var audio = $("#audio")[0];
 $("nav-one a").mouseenter(function() {
   audio.play();
 });
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
